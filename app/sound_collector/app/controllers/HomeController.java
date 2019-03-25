5271567a28143b7e764df30f3aaa7e5cb69220cc
Oren Spiegel <ospiegel51191@gmail.com>
2019-03-25 22:13:32 +0200

    MVP all queries accounted for

--- app/sound_collector/app/controllers/HomeController.java
+++ app/sound_collector/app/controllers/HomeController.java
@@ -4,40 +4,32 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
-import com.mongodb.QueryBuilder;
-import com.mongodb.ServerAddress;
+
+
 
 import models.NodeBean;
-import models.Sound;
 import models.ContinuousSound;
 import models.SporadicSound;
 import models.SoundFactory;
 import play.libs.Json;
 import play.mvc.*;
-import java.util.Date;
+
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 
 
 
-/**
- * This controller contains an action to handle HTTP requests to the
- * application's home page.
- */
+
 public class HomeController extends Controller {
 	
-	/**
-	 * An action that renders an HTML page with a welcome message. The configuration
-	 * in the <code>routes</code> file means that this method will be called when
-	 * the application receives a <code>GET</code> request with a path of
-	 * <code>/</code>.
-	 */
 	
 	MongoClient mMongoClient;
 	DB mDB;
@@ -109,9 +101,56 @@ public class HomeController extends Controller {
 				e.printStackTrace();
 				return ok("Something went terribly wrong --- please check your query type");
 			}
+		}else if(queryType.equals("contSoundsIntersect")){
+			try {
+				query = new BasicDBObject("endTime", new BasicDBObject("$gt", startTime).append("$lte", endTime));
+				List<DBObject> sounds = contSound.getContinuousSounds(query);
+				List<DBObject> listOfsoundsWithIntersects = new ArrayList<DBObject>();
+				for (DBObject sound : sounds) {
+					Object start = getFieldFromCursor(sound,"startTime");
+					Object end = getFieldFromCursor(sound,"endTime");
+					System.out.println("---start:  "+start+"---end:  "+end);
+					DBObject entry =  new BasicDBObject();
+					entry.put("sound", sound);
+					BasicDBObject subquery = null;
+					subquery = new BasicDBObject("startTime", new BasicDBObject("$gt", start).append("$lte", end));
+					List<DBObject> sporadicSounds = spoSound.getSporadicSounds(subquery);
+					ObjectMapper objectMapper = new ObjectMapper();
+					String arrayToJson;
+					arrayToJson = objectMapper.writeValueAsString(sporadicSounds);
+					entry.put("interestingSporadic", arrayToJson);
+					listOfsoundsWithIntersects.add(entry);
+					System.out.println(entry);
+				}
+				
+				ObjectMapper objectMapper = new ObjectMapper();
+				String arrayToJson;
+				arrayToJson = objectMapper.writeValueAsString(listOfsoundsWithIntersects);
+				result.put("continuous sounds with intersects", arrayToJson);
+				return ok(result);
+			} catch (JsonProcessingException e) {
+				e.printStackTrace();
+				return ok("Something went terribly wrong --- please check your query type");
+			}
 		}else {
 			return ok("Something went terribly wrong --- please check your query type");
 		}	
+	}
+	
+	
+	private Object getFieldFromCursor(DBObject o, String fieldName) {
+
+	    final String[] fieldParts = StringUtils.split(fieldName, '.');
+
+	    int i = 1;
+	    Object val = o.get(fieldParts[0]);
+
+	    while(i < fieldParts.length && val instanceof DBObject) {
+	        val = ((DBObject)val).get(fieldParts[i]);
+	        i++;
+	    }
+
+	    return val;
 	}
 
 
--- app/sound_collector/build.sbt
+++ app/sound_collector/build.sbt
@@ -12,8 +12,13 @@ libraryDependencies += guice
 libraryDependencies ++= Seq(
 	"org.mongodb" % "mongo-java-driver" % "3.10.1",
     "org.mongodb" % "mongodb-driver-sync" % "3.10.1",
-    "org.mongodb.morphia" % "morphia" % "0.107"
+    "org.mongodb.morphia" % "morphia" % "0.107",
 )
+// Visit https://mvnrepository.com/artifact/commons-codec/commons-codec to see the list of versions available
+libraryDependencies += "commons-codec" % "commons-codec" % "1.11"
+
+// Visit https://mvnrepository.com/artifact/org.apache.commons/commons-lang3 to see the list of versions available
+libraryDependencies += "org.apache.commons" % "commons-lang3" % "3.8.1"
 
 EclipseKeys.preTasks := Seq(compile in Compile, compile in Test)
 
